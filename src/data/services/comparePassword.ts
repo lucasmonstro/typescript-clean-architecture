85c735e9dace3109d9032f00a7bf751d0649d3b3
lucasmonstro <joao.galiano.silva@gmail.com>
2022-12-16 10:27:38 +0000

    refactor: remove eslint ignores

--- src/data/services/comparePassword.ts
+++ src/data/services/comparePassword.ts
@@ -4,7 +4,6 @@ import { ComparePasswordsService } from '@/core/services';
 export const makeComparePasswordsService =
   (): ComparePasswordsService =>
   (password: string, encryptedPassword: string): boolean => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
     const passwordsAreEqual: boolean = compareSync(password, encryptedPassword);
     return passwordsAreEqual;
   };
